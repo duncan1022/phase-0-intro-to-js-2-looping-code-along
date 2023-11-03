2ab629178a523c17d8b822309ca9cf53996d412c
brittany duncan <duncandakota08@gmail.com>
2023-11-03 02:39:37 -0400

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event) {
+  let gift = [];
+  event = "surprise";
+  for (let i = 0; i < names.length; i++) {
+    gift[i] = 'Thank you, ' + names[i] + ', for the wonderful ' + event + ' gift!';
+  }
+  return gift;
+}
+
+let number = 10;
+function countDown (number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
